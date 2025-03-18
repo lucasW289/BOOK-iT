84550d2cee7c6b3c7913382c7b0bbd2256e7b26e
Aung Phyo Linn <aungphyolinn289@gmail.com>
2025-03-18 15:41:14 +0700

    Added Hotel Reg feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,16 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -30,7 +38,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 // Define API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 // Start the server
 app.listen(1111, () => {
   console.log("Server is running on port 1111");
---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
+
+const AddHotel = () => {
+  const { showToast } = useAppContext();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.addMyHotel,
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isPending={isPending} />;
+};
+
+export default AddHotel;
