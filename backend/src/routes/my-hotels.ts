87cb39566a3175b3b3d7e4da6f0ff3c8723a50ff
Aung Phyo Linn <aungphyolinn289@gmail.com>
2025-03-19 13:11:38 +0700

    Added View Hotels Feauture

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -68,4 +68,13 @@ async function uploadImages(imageFiles: Express.Multer.File[]) {
   return imageUrls;
 }
 
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ userId: req.userId });
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 export default router;
